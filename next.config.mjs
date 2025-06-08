764f8b562b57b802825fa13317aa25a63beea8fc
azizkhan1988 <azizurrehmankhan321@gmail.com>
2025-06-08 20:28:12 +0500

    asdasds

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-  },
-}; 
-
-
+const nextConfig = {};
 
 export default nextConfig;
